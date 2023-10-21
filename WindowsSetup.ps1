d171606f286dfab5c943f1a9bb207eb6e0271a32
TheOncher <oriori057@gmail.com>
2023-10-21 18:33:37 +0300

    Cleaned up Code and Restructured
    Cleaned up Code and Restructured

--- WindowsSetup.ps1
+++ WindowsSetup.ps1
@@ -2,12 +2,13 @@
 #Requires -PSEdition Desktop
 
 # Constants
-$DomRecVersion = "DomRec3 v1"
-$DatabasesList = [ordered]@{"1" = "PostgresSQL"; "2" = "MongoDB"; "3" = "MicrosoftSQL"; "4" = "MySQL"; "5" = "SQLite" }
-$DockerServiceNameDefault = "com.docker.service"
-$DockerServiceName = "com.docker.service"
-$ScriptName = Split-Path -Path $($MyInvocation.InvocationName) -Leaf
-$BackendDatabaseConnector = "BE Service - Database Connector"
+$ConstDomRecVersion = "DomRec3 v1"
+$ConstDockerServiceName = "com.docker.service"
+$ConstDockerServiceNameDefault = "com.docker.service"
+$ConstScriptName = Split-Path -Path $($MyInvocation.InvocationName) -Leaf
+$ConstContainerName_BackendDatabaseConnector = "BE Service - Database Connector"
+$ConstContainerName_FrontendGUI = "FE Service - GUI"
+$ConstDatabasesList = [ordered]@{"1" = "PostgresSQL"; "2" = "MongoDB"; "3" = "MicrosoftSQL"; "4" = "MySQL"; "5" = "SQLite" }
 
 # ------------------------------------------------------------------------- #
 
@@ -15,6 +16,54 @@ $BackendDatabaseConnector = "BE Service - Database Connector"
 Function Test-Administrator {  
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+# Function Check-Dependencies
+Function Check-Dependencies {
+    Clear-Host
+    # Check Docker Service Status
+    $DockerServiceStatus = (Get-Service -Name "$ConstDockerServiceName" -ErrorAction SilentlyContinue ).Status
+
+    # Docker not Installed, exit WindowsSetup.ps1
+    if ($null -eq $DockerServiceStatus) {
+        Write-Host "Looks Like Docker is not installed, Please refer to the Docker website and install it first `nAnd make sure the service name is '$ConstDockerServiceNameDefault', if you have modified the Docker service name, `ncheck the $ConstScriptName code and modify the 'DockerServiceName' variable in the Constants section"
+        Write-Host "Click anything to exit"
+        $Host.UI.ReadLine()
+        exit
+    }
+
+    # Check if Docker is Running
+    while ($DockerServiceStatus -ne "Running") {
+        $Answer = (Read-Host -Prompt "Docker Service isnt Running, Do you want to start it? [Y/n]").ToLower()
+
+        # User doesnt want to start docker service, then exit
+        if ($Answer -eq "n") { 
+            Write-Host "Well Then, Cheers!"; $Host.UI.ReadLine(); exit 
+        }
+
+        # Resubmit answer
+        elseif (($Answer -ne "y") -and ($Answer -ne "")) {}
+
+        # Request admin access to start the service
+        elseif ( (($Answer -eq "y") -or ($Answer -eq "")) -and (!(Test-Administrator)) ) {
+            $StartProcessArguments = '-c "Start-Service -Name ' + $ConstDockerServiceName + '"'
+            Start-Process -FilePath "powershell.exe" -ArgumentList $StartProcessArguments -Verb RunAs
+        }
+
+        # Attempt to start Docker
+        elseif ( (($Answer -eq "y") -or ($Answer -eq "")) -and (Test-Administrator) ) {
+            try { Start-Service -Name "$ConstDockerServiceName" }
+            catch { Write-Host "Cannot Start Docker Service, Please try Manually"; $Host.UI.RTeadLine(); exit }
+        }
+
+        # ReChecking for docker service status
+        Start-Sleep -Seconds 4 
+        $DockerServiceStatus = (Get-Service -Name "$ConstDockerServiceName" -ErrorAction SilentlyContinue ).Status
+    }
+
+    # Docker is up!
+    Write-Host "Docker is up!"
+    Clear-Host
 }
 
 # Function Get-DatabaseListInfo
@@ -59,55 +108,13 @@ Function Get-DatabaseListInfo {
     }
 }
 
-# Function Check-Dependencies
-Function Check-Dependencies {
-    Clear-Host
-    # Check Docker Service Status
-    $DockerServiceStatus = (Get-Service -Name "$DockerServiceName" -ErrorAction SilentlyContinue ).Status
-
-    # Docker not Installed
-    if ($null -eq $DockerServiceStatus) {
-        Write-Host "Looks Like Docker is not installed, Please refer to the Docker website and install it first `nAnd make sure the service name is '$DockerServiceNameDefault', if you have modified the Docker service name, `ncheck the $ScriptName code and modify the 'DockerServiceName' variable in the Constants section"
-        Write-Host "Click anything to exit"
-        $Host.UI.ReadLine()
-        exit
-    }
-
-    # Check if Docker is Running
-    while ($DockerServiceStatus -ne "Running") {
-        $Answer = (Read-Host -Prompt "Docker Service isnt Running, Do you want to start it? [Y/n]").ToLower()
-        # User doesnt want to start docker service, then exit
-        if ($Answer -eq "n") { 
-            Write-Host "Well Then, Cheers!"; $Host.UI.ReadLine(); exit 
-        }
-        # Resubmit answer
-        elseif (($Answer -ne "y") -and ($Answer -ne "")) {}
-        # Request admin access to start the service
-        elseif ( (($Answer -eq "y") -or ($Answer -eq "")) -and (!(Test-Administrator)) ) {
-            $StartProcessArguments = '-c "Start-Service -Name ' + $DockerServiceName + '"'
-            Start-Process -FilePath "powershell.exe" -ArgumentList $StartProcessArguments -Verb RunAs
-        }
-        # Attempt to start Docker
-        elseif ( (($Answer -eq "y") -or ($Answer -eq "")) -and (Test-Administrator) ) {
-            try { Start-Service -Name "$DockerServiceName" }
-            catch { Write-Host "Cannot Start Docker Service, Please try Manually"; $Host.UI.RTeadLine(); exit }
-        }
-        # ReChecking for docker service status
-        Start-Sleep -Seconds 4 
-        $DockerServiceStatus = (Get-Service -Name "$DockerServiceName" -ErrorAction SilentlyContinue ).Status
-    }
-    # Docker is up!
-    Write-Host "Docker is up!"
-    Clear-Host
-}
-
 # Function Invoke-DatabaseConnector
 Function Invoke-DatabaseConnector {
     # Starting Database Connector Explanation, How to Configure
-    Write-Host "At First you need to set up your Database `n$DomRecVersion Supports $($DatabasesList.Count) Different Databases, `n$(Get-DatabaseListInfo -DBListInString $True) `nYou can set up one of each and they will work simultaneously, `nAll Data will be saved to all of them for back up"
+    Write-Host "At First you need to set up your Database `n$ConstDomRecVersion Supports $($ConstDatabasesList.Count) Different Databases, `n$(Get-DatabaseListInfo -DBListInString $True) `nYou can set up one of each and they will work simultaneously, `nAll Data will be saved to all of them for back up"
 
     # Acquiring DB Credentials Json from the Backend Database Connector Service
-    $DBCredentials = (Get-Content -Path "$PSScriptRoot\\$BackendDatabaseConnector\\Configuration\\DBCredentials.json" | ConvertFrom-Json)
+    $DBCredentials = (Get-Content -Path "$PSScriptRoot\\$ConstContainerName_BackendDatabaseConnector\\Configuration\\DBCredentials.json" | ConvertFrom-Json)
 
     # Starting the Databases Configuration
     #While ($DatabaseNumber)
